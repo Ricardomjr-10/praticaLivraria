90332a9443962a0534dcc477532d4c22f616f0d4
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-10 22:15:12 -0300

    build:conexao

---
+++ conexao.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql')
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'dba',
+    password: '1234',
+    database: 'livraria'
+})
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log('Conectado com sucesso!')
+    }
+})
