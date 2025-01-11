7026e9bd69eef9fe82a024106789cc2e400993a3
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-10 22:32:31 -0300

    build:dir static

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+//rota estatica
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
