7ab2ec41abd641cece962b52e5e304791f4105d0
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-11 22:26:20 -0300

    bug

--- pucblic/app.js
+++ pucblic/app.js
@@ -6,5 +6,23 @@ const formFornecedor = document.getElementById('formFornecedor')
 const formPeca = document.getElementById('formPeca')
 const formMontagem = document.getElementById('formMontagem')
 
+formAutor.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(formAutor);
+
+    fetch('/cadAutor', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('mensagens').innerHTML = data
+    })
+})
+
 
 
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 import express from 'express'
+import conexao from './conexao.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('./pucblic'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/cadAutor', (req, res) => {
+   const { name } = req.body
+   const sql = 'INSERT INTO autores (name) VALUES (?)'
+   conexao.query(sql, [name], (err, result) => {
+       if (err) {
+           console.error('Erro ao cadastrar autor:', err)
+           res.status(500).send('Erro ao cadastrar autor')
+       } else {
+           console.log('Autor cadastrado com sucesso!')
+           res.send('Autor cadastrado com sucesso!')
+       }
+   })
 })
 
 app.listen(PORT, () => {
