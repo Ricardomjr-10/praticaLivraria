78d4950c1a20e2ca7aa4573f98a191e739f3efcd
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-13 21:08:41 -0300

    get all

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/all', (req, res) => {
+    conexao.query('SELECT * FROM livraria', (err, result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
 app.post('/cadAutor', (req, res) => {
     const { nomeAutor } = req.body
     const sql = `INSERT INTO autores (name) VALUES ('${nomeAutor}')`
@@ -24,6 +34,18 @@ app.post('/cadAutor', (req, res) => {
             res.send(err)
         } else {
             res.json('Autor cadastrado com sucesso!')
+        }
+    })
+})
+
+app.post('/cadLivro', (req, res) => {
+    const { tituloLivro, autorLivro } = req.body
+    const sql = `INSERT INTO livros (title, author_id) VALUES ('${tituloLivro}', ${autorLivro})`
+    conexao.query(sql, (err, result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.json('Livro cadastrado com sucesso!')
         }
     })
 })
