5caf7c55c5514ebb755970a0600368f263f2cc18
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-15 20:56:30 -0300

    fix: add column conta em fornecedores

--- conexao.js
+++ conexao.js
@@ -14,3 +14,8 @@ export default conexao
 
 //criar coluna cnpj em fornecedores
 //conexao.query('ALTER TABLE fornecedores ADD COLUMN cnpj VARCHAR(14)')
+
+//apagar tabela conta se existir
+// conexao.query('DROP TABLE contas')
+
+conexao.query('ALTER TABLE fornecedores ADD COLUMN contas INT NOT NULL')
